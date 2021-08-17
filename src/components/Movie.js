bb55a5049a716df335b6e1c3019ade36531f18e5
QuanLyThamQuan <55777063+NguyenTranThanhTruc@users.noreply.github.com>
2021-08-17 11:06:07 +0700

    fetch movie data from api

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const Movie = () => <div>Movie</div>;
+// config
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+
+// component
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+
+// hook
+import { useMovieFetch } from "../hooks/useMovieFetch";
+
+// image
+import NoImage from "../images/no_image.jpg";
+
+const Movie = () => {
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+
+  console.log(movie);
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  );
+};
 
 export default Movie;
